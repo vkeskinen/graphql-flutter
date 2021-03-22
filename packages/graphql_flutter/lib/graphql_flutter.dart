b9c94eb1828cde0d126b9be9534e5e6d6535781a
Vesa Satama <vesa.satama@nautics.fi>
2021-03-22 13:48:50 +0200

    Deleted subscription widgets

--- packages/graphql_flutter/lib/graphql_flutter.dart
+++ packages/graphql_flutter/lib/graphql_flutter.dart
@@ -10,4 +10,3 @@ export 'package:graphql_flutter/src/widgets/graphql_consumer.dart';
 export 'package:graphql_flutter/src/widgets/graphql_provider.dart';
 export 'package:graphql_flutter/src/widgets/mutation.dart';
 export 'package:graphql_flutter/src/widgets/query.dart';
-export 'package:graphql_flutter/src/widgets/subscription.dart';
